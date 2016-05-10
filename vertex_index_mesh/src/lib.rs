7cf510a3124843cc8872f61087104f78ec163b89
Mark Hintz <markohintz@gmail.com>
2016-05-10 14:51:05 +0200

    Pull the recompute_normals function out of vertex_index_mesh

--- vertex_index_mesh/src/lib.rs
+++ vertex_index_mesh/src/lib.rs
@@ -9,3 +9,30 @@ mod vertex;
 pub use vertex_index_mesh::VertexIndexMesh;
 pub use bufferset::BufferSet;
 pub use vertex::Vertex;
+
+use cgmath::prelude::*;
+
+pub fn recompute_normals(mut mesh: VertexIndexMesh) -> VertexIndexMesh {
+  for tri in mesh.indices.chunks(3) {
+    if tri.len() != 3 { continue; }
+    let (i0, i1, i2) = (tri[0] as usize, tri[1] as usize, tri[2] as usize);
+
+    let apex = mesh.vertices[i0].pos();
+    let s0 = apex - mesh.vertices[i1].pos();
+    let s1 = apex - mesh.vertices[i2].pos();
+
+    let normal = s0.cross(s1).normalize();
+
+    for & i in & [i0, i1, i2] {
+      let norm_i = mesh.vertices[i].normal();
+      mesh.vertices[i].set_normal(norm_i + normal);
+    }
+  }
+
+  for vert in mesh.vertices.iter_mut() {
+    vert.normalize_normal();
+  }
+
+  mesh
+}
+
--- vertex_index_mesh/src/vertex_index_mesh.rs
+++ vertex_index_mesh/src/vertex_index_mesh.rs
@@ -42,28 +42,6 @@ impl VertexIndexMesh {
     }
   }
 
-  pub fn recompute_normals(&mut self) {
-    for tri in self.indices.chunks(3) {
-      if tri.len() != 3 { continue; }
-      let (i0, i1, i2) = (tri[0] as usize, tri[1] as usize, tri[2] as usize);
-
-      let apex = self.vertices[i0].pos();
-      let s0 = apex - self.vertices[i1].pos();
-      let s1 = apex - self.vertices[i2].pos();
-
-      let normal = s0.cross(s1).normalize();
-
-      for & i in & [i0, i1, i2] {
-        let norm_i = self.vertices[i].normal();
-        self.vertices[i].set_normal(norm_i + normal);
-      }
-    }
-
-    for vert in self.vertices.iter_mut() {
-      vert.normalize_normal();
-    }
-  }
-
   pub fn to_buffer<T: Facade>(& self, gl: & T) -> BufferSet {
     BufferSet::from_vertex_index(gl, self.primtype, self)
   }
