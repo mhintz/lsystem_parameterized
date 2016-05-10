6cd7f9c2b3645adf99afe424745b4ff40d965eb4
Mark Hintz <markohintz@gmail.com>
2016-05-10 14:51:13 +0200

    Add assign_colors function

--- vertex_index_mesh/src/lib.rs
+++ vertex_index_mesh/src/lib.rs
@@ -11,6 +11,7 @@ pub use bufferset::BufferSet;
 pub use vertex::Vertex;
 
 use cgmath::prelude::*;
+use cgmath::Vector4;
 
 pub fn recompute_normals(mut mesh: VertexIndexMesh) -> VertexIndexMesh {
   for tri in mesh.indices.chunks(3) {
@@ -36,3 +37,19 @@ pub fn recompute_normals(mut mesh: VertexIndexMesh) -> VertexIndexMesh {
   mesh
 }
 
+pub fn assign_colors<F>(mut mesh: VertexIndexMesh, get_colors: F) -> VertexIndexMesh where
+F: Fn((usize, usize, usize), (& Vertex, & Vertex, & Vertex)) -> [Vector4<f32>; 3]
+ {
+  for tri in mesh.indices.chunks(3) {
+    if tri.len() != 3 { continue; }
+    let (i0, i1, i2) = (tri[0] as usize, tri[1] as usize, tri[2] as usize);
+
+    let colors = get_colors((i0, i1, i2), (& mesh.vertices[i0], & mesh.vertices[i1], & mesh.vertices[i2]));
+
+    for (i, & vindex) in [i0, i1, i2].into_iter().enumerate() {
+      mesh.vertices[vindex].set_color(colors[i]);
+    }
+  }
+
+  mesh
+}
